4140bf887ef749b184e9067027e341dcba3fb9c7
Ximena Garcia <ximenascoding@gmail.com>
2025-12-05 09:47:02 -0600

    Refactor counting logic and fix include statement
    Fix include directive and update counting logic to ask user for input.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#inlcude <string>
 using namespace std;
 
+
 int main(){
+  int times, counter;
+  
   cout << "Hello world" << endl;
-  for (int i = 0; i < 10; i++){
-    cout << i;
+  
+  // ask how many times
+  cout << "How many times do you want to count until 10?" << endl;
+  cin >> times;
+
+  
+  counter = 0;
+  while (counter < times){
+    //Cuenta del 1 al 10
+    for (int i = 0; i < 10; i++){
+      cout << i << endl;
+    }
+      counter++;
   }
+  
   return 0;
 }
